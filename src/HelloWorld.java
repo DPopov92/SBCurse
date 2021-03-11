74b7091106fb4100b32d9602afd4a7dfc05862a8
Denis <denpopov192@gmail.com>
2021-03-11 16:09:29 +0300

    Simple test v2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
-public class hello {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("asd");
-        String x= "0"  ;
+        System.out.println("HelloWorld");
+        String x = "0"  ;
         if (x != null) {
             x =  null;
         }
