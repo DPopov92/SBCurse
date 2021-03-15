0ed402cc03fdacaf2dd7c4fe793d1d901714f0ba
Denis <denpopov192@gmail.com>
2021-03-15 23:18:46 +0300

    HW lesson 2

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        String x = "0"  ;
-        if (x != null) {
-            x =  null;
-        }
-    }
-}
